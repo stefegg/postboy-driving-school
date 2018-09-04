7e8f5a8765d1f482993c0151b9c599f14917a6f1
Stef Egbert <stephenegbert@gmail.com>
2018-09-04 10:44:36 -0400

    morning

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -6,6 +6,9 @@ def full_name
   self.name + " " + self.last_name
 end
 
+def full_address
+  self.address + " " + self.city + " " + self.zip.to_s
+
 def generate_aid
   self.aid = SecureRandom.hex(3).upcase
 end
